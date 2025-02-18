ee8066d1c474605b26907d466175f7088edb429c
sherstanoi <nikitasherstenikin@mail.ru>
2025-02-18 16:37:43 +0300

    yes

--- Lab_1.cpp
+++ Lab_1.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    std::cout << "Я умею возводить факториалы! смотри 34! ";
+    std::cout << "Я умею возводить факториалы! смотри Число! ";
     std::cout << mymath::factorial(10) << std::endl;
 
     return 0;
