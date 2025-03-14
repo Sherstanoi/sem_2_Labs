2c2d978a9b174aec7628a5f64d789d69a0d6c40d
sherstanoi <nikitasherstenikin@mail.ru>
2025-03-14 15:23:25 +0300

    does it work?

--- mymath/CMakeFiles/mymath.dir/mymath.cpp.o.d
+++
@@ -1,4 +0,0 @@
-mymath/CMakeFiles/mymath.dir/mymath.cpp.o: \
- /home/nikita/Sherst_2_Sem_Labs/sem_2_Labs/mymath/mymath.cpp \
- /usr/include/stdc-predef.h \
- /home/nikita/Sherst_2_Sem_Labs/sem_2_Labs/mymath/mymath.h
