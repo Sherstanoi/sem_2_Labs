ade8522eff022927ce4213622d414eca6eae63eb
sherstanoi <nikitasherstenikin@mail.ru>
2025-03-19 13:46:21 +0300

    message

--- Lab_3.cpp
+++ Lab_3.cpp
@@ -1,5 +1,103 @@
 #include "Lab_3.hpp"
 
-Drob() {
-    
+Drob::Drob() {
+    numerator = 1;
+    denominator = 1;
+    FullPiece = 0;
+    Unturned = new char;
+}
+Drob::Drob(int num, int den) {
+    numerator = num;
+    denominator = den;
+    FullPiece = 0;
+    Unturned = new char;
+}
+Drob::Drob(double frac) {
+    numerator = 1;
+    denominator = 1;
+    FullPiece = frac;
+    Unturned = new char;
+}
+
+Drob::Drob(const char* unturn) {
+    numerator = 1;
+    denominator = 1;
+    FullPiece = 0;
+    *Unturned = *unturn;
+}
+
+void Drob::SetUnturned(char* betta) {
+    Unturned = new char[std::strlen(betta)];
+    Unturned = betta;
+}
+void Drob::SetDrob(int num, int den) {
+
+}
+
+void Drob::ChangeDrob() {
+    int tempNum= 0;
+    int tempDen = 0;
+    bool Flag = true;
+    int schet = 0;
+    while(Flag) {
+        if(Unturned[schet] != '/'){
+            tempNum = tempNum*10 + Unturned[schet];
+            ++schet;
+        }
+        else{
+            Flag = false;
+        }
+    }
+    Flag = true;
+    schet = std::strlen(Unturned)-1;
+    while(Flag) {
+        if(Unturned[schet] != '/'){
+            tempDen = tempDen*10 + Unturned[schet];
+            --schet;
+        }
+        else{
+            Flag = false;
+        }
+    }
+    numerator = tempNum;
+    denominator = tempDen;
+}
+
+Drob& Drob::operator=(const Drob& alpha) {
+    return *this;
+}
+Drob& Drob::operator+(const Drob& alpha) {
+    numerator += alpha.numerator;
+    return *this;
+}
+Drob& Drob::operator-(const Drob& alpha) {
+    numerator -= alpha.numerator;
+    return *this;
+}
+Drob& Drob::operator*(const Drob& alpha) {
+    numerator *= alpha.numerator;
+    denominator *= alpha.denominator;
+    return *this;
+}
+Drob& Drob::operator/(const Drob& alpha) {
+    numerator /= alpha.numerator;
+    denominator /= alpha.denominator;
+    return *this;
+}
+
+std::ostream& operator<<(std::ostream& out,const Drob& alpha) {
+    std::cout << alpha.numerator << '/' << alpha.denominator;
+    return out;
+}
+std::istream& operator>>(std::istream& in, Drob& alpha) {
+    char betta[100000000];
+    in >> betta;
+    // delete[] alpha.Unturned;
+    // alpha.SetUnturned(betta);
+    // alpha.ChangeDrob();
+    return in;
+}
+
+void ChangeDrob() {
+
 }
--- Lab_3.hpp
+++ Lab_3.hpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
 
 class Drob {
     private:
 
     int numerator = 1;
     int denominator = 1;
-    int FullPiesce = 0;
+    int FullPiece = 0;
     char* Unturned;
 
     public:
@@ -15,15 +16,23 @@ class Drob {
     Drob(double frac);
     Drob(int num, int den);
     Drob(Drob& alpha);
+    Drob(const char* unturn);
+
     void SetDrob(int num, int den);
 
-    friend void ChangeDrob(Drob& alpha);
+    void ChangeDrob();
 
-    friend Drob& operator+(const Drob& alpha,const Drob& Betta);
-    friend Drob& operator-(const Drob& alpha,const Drob& Betta);
-    friend Drob& operator*(const Drob& alpha,const Drob& Betta);
-    friend Drob& operator/(const Drob& alpha,const Drob& Betta);
-    friend std::ostream& operator<<(std::ostream& out,const Drob& alpha);
-    friend std::istream& operator+(std::istream& in,const Drob& Betta);
     Drob& operator=(const Drob& alpha);
+    Drob& operator+(const Drob& alpha);
+    Drob& operator-(const Drob& alpha);
+    Drob& operator*(const Drob& alpha);
+    Drob& operator/(const Drob& alpha);
+
+    friend std::ostream& operator<<(std::ostream& out,const Drob& alpha);
+    friend std::istream& operator>>(std::istream& in,Drob& alpha);
+
+    ~Drob() {
+        delete[] Unturned;
+    }
+    void SetUnturned(char* betta);
 };
