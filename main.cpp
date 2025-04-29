54ed404fc9a526ca514602832d0d93f8ce5243fd
sherstanoi <nikitasherstenikin@mail.ru>
2025-04-29 15:59:27 +0300

    end

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include "lab_4.hpp"
 
 int main() {
+  MyStack<char> CharStack;
+  MyStack<char> CharStackSecond;
+  CharStack.PushElement('a');
+  CharStack.PushElement('b');
+  CharStack.PushElement('c');
+  CharStackSecond.PushElement('f');
+  CharStackSecond.PushElement('e');
+  CharStackSecond.PushElement('d');
+  MyStack<char> CharStackThird(CharStack);
+  MyStack<char> CharStackFourd = CharStackSecond;
+  std::cout<<"CharStackA:"<<CharStackThird << "\n";
+  std::cout<<"CharStackB:"<<CharStackFourd << "\n";
+  std::cout<<"Sum A+B:" << CharStackSecond + CharStack << "\n";
     Functions::StartProgramm();
 
   return 0;
