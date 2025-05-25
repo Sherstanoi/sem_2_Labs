9e176f310438598638e4aa030afd7cacc021551c
sherstanoi <nikitasherstenikin@mail.ru>
2025-05-25 20:54:00 +0300

    little mistake

--- lab7.hpp
+++ lab7.hpp
@@ -6,12 +6,6 @@
 #pragma once
 
 namespace Start{
-    enum class ItemType {
-        Toy = 1,
-        MilkProduct = 2,
-        ProductType= 3
-    };
-
     void Menu();
     void AddElement(Stack<Item*>& Products);
     void PrintStack(Stack<Item*>& Products);
